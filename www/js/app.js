ff6dc4be34287b6915f83025fdb649ede3579f41
Saqib Kayani <m.s.kayani@edu.salford.ac.uk>
2015-10-25 02:30:18 +0000

    ADD functionality to append API data to html markup.

--- www/js/app.js
+++ www/js/app.js
@@ -17,8 +17,28 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     onDeviceReady: function() {
         document.getElementById("entry").addEventListener('change', onEntryChange, false);
-        init();
+
     }
 };
-]
+
+init = {
+	update : function(){
+		$.ajax({
+		url: 'http://stealapi.apphb.com/api/Crime/GetARandomCrime',
+		dataType: 'jsonp',
+		contentType: "application/json; charset=utf-8",
+		success: function(json) {
+			var jsonObject = json;
+			alert("we got the data!!!");
+			console.log(jsonObject);	
+			$('[name="api"]').append('<p>'+ jsonObject.Id +'</p>');
+			$('[name="api"]').append('<p>'+ jsonObject.PersistentId +'</p>');
+			$('[name="api"]').append('<p>'+ jsonObject.Location.Longitude +'</p>');
+			$('[name="api"]').append('<p>'+ jsonObject.Location.Latitude +'</p>');
+			$('[name="api"]').append('<p>'+ jsonObject.Category +'</p>');
+		}
+	});
+			//
+	}
+};
 
